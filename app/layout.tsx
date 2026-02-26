b191992b19fde2027cd5abc66fb4b63e2d0c9bbc
HarryDayOf <harry@dayofweddings.com>
2026-02-25 19:34:44 -0500

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, padding: 0, fontFamily: '-apple-system, sans-serif' }}>
-        {children}
-      </body>
-    </html>
-  );
-}
